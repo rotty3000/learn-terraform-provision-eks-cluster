f8b32d2ea57dd1a872ca1ef421e1bb0aa868b446
Raymond Augé <raymond.auge@liferay.com>
2025-02-13 18:01:24 -0500

    update helm chart

--- helm.tf
+++ helm.tf
@@ -48,7 +48,7 @@ resource "helm_release" "liferay" {
   namespace         = "liferay"
   repository        = "oci://ghcr.io/liferaycloud/liferay-helm-chart/charts"
   reuse_values      = true
-  version           = "0.1.33"
+  version           = "0.1.34"
 
   values = [
     file("${path.module}/values-aws.yaml")
